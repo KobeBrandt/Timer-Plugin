1064f2e3e5204a954d4db6e044b306e625463f3d
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2025-12-01 14:12:14 +0100

    Add initial TimerPlugin project files
    Initial commit for the TimerPlugin, including solution, project, source files, editor config, build props, plugin logic, timer action, helpers, packaging metadata, event mappings, and VS Code configuration. Implements a timer action with haptic feedback, logging, and resource management for Loupedeck devices.

---
+++ TimerPlugin/src/Actions/Timer.cs
@@ -0,0 +1,74 @@
+﻿namespace Loupedeck.TimerPlugin.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Timers;
+
+    public class Timer : ActionEditorCommand
+    {
+        private const String EventName = "buttonPress";
+        private System.Timers.Timer _delayTimer;
+
+        private Int32 _selectedTime = 0;
+        public Timer()
+        {
+            // Set basic properties
+            this.Name = "Timer";
+            this.DisplayName = "Timer";
+            this.GroupName = "Timers";
+            this.Description = "A timer that uses the haprics when finished";
+
+            // Add controls for user configuration
+            this.ActionEditor.AddControlEx(
+    new ActionEditorSlider(name: "Time", labelText: "Time (sec):", description: "Adjust time")
+      .SetValues(minimumValue: 1, maximumValue: 60, defaultValue: 15, step: 1));
+
+            // Subscribe to events
+            this.ActionEditor.ControlValueChanged += this.OnControlValueChanged;
+        }
+
+        private void OnControlValueChanged(Object sender, ActionEditorControlValueChangedEventArgs e)
+        {
+            if (e.ControlName.EqualsNoCase("Time"))
+            {
+                this._selectedTime = (Int32)e.ActionEditorState.GetControlValue("Time").ParseInt32();
+
+                // Update display name based on user input
+                e.ActionEditorState.SetDisplayName($"Timer:\n {this._selectedTime}");
+
+                PluginLog.Info("Starting setting up timer");
+                Int32 timeInSeconds = this._selectedTime * 1;
+                this._delayTimer = new System.Timers.Timer(timeInSeconds * 1000); // time in milliseconds
+                this._delayTimer.Elapsed += OnDelayElapsed;
+                this._delayTimer.AutoReset = false; // One-shot timer
+                PluginLog.Info("Time set for: " + timeInSeconds * 1000);
+            }
+        }
+
+        protected override Boolean OnLoad()
+        {
+            this.Plugin.PluginEvents.AddEvent(EventName, "Button Press", "This haptic event is sent when the user presses the button");
+            return true;
+        }
+
+        protected override Boolean RunCommand(ActionEditorActionParameters actionParameters)
+        {
+            PluginLog.Info("button pressed");
+            PluginLog.Info("Starting timer");
+
+            this._delayTimer.Stop(); // Stop any existing timer
+            this._delayTimer.Start();
+            return true;
+
+        }
+        private void OnDelayElapsed(object sender, ElapsedEventArgs e)
+        {
+            PluginLog.Info("Starting haptics");
+            // Trigger event on UI thread if needed, but RaiseEvent should be thread-safe
+            this.Plugin.PluginEvents.RaiseEvent(EventName);
+        }
+    }
+}
